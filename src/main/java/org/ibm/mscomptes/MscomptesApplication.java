3aa6354feca664a1a7a408019b23ae0995f28d05
KORAICHI Mossaab <mosaab.koraichi@ibm.com>
2019-06-02 17:33:19 +0000

    init

---
+++ src/main/java/org/ibm/mscomptes/MscomptesApplication.java
@@ -0,0 +1,13 @@
+package org.ibm.mscomptes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscomptesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscomptesApplication.class, args);
+    }
+
+}
