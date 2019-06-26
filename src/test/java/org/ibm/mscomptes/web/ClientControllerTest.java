40d6a5c8c2ab76998e077265386fa24128de001e
OumaimaBjd <oumaimaboujadi10@gmail.com>
2019-06-26 10:49:02 +0100

    unit test added ---------------- without a deep functionality testing

---
+++ src/test/java/org/ibm/mscomptes/web/ClientControllerTest.java
@@ -0,0 +1,74 @@
+package org.ibm.mscomptes.web;
+
+import org.ibm.mscomptes.entities.Client;
+import org.junit.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.List;
+import static java.util.Collections.singletonList;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@WebMvcTest(ClientController.class)
+public class ClientControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ClientController controller;
+
+    @Test
+    public void getAll() throws Exception {
+        Client o = new Client();
+        o.setNom("Amine");
+        List<Client> allClients = singletonList(o);
+        given(controller.getAll()).willReturn(allClients);
+
+        mvc.perform(get("/clients")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].nom", is(o.getNom())));
+    }
+
+
+    @Test
+    public void newOne()  throws Exception {
+        Client o = new Client();
+        o.setNom("Amine");
+        List<Client> allClients = singletonList(o);
+        given(controller.getAll()).willReturn(allClients);
+
+        mvc.perform(get("/clients")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].nom", is(o.getNom())));
+    }
+
+    @Test
+    public void save()  throws Exception {
+        Client o = new Client();
+        o.setNom("Amine");
+        List<Client> allClients = singletonList(o);
+        given(controller.getAll()).willReturn(allClients);
+
+        mvc.perform(get("/clients")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].nom", is(o.getNom())));
+    }
+}
---
+++ src/test/java/org/ibm/mscomptes/web/CompteControllerTest.java
@@ -0,0 +1,130 @@
+package org.ibm.mscomptes.web;
+
+import org.ibm.mscomptes.entities.Client;
+import org.ibm.mscomptes.entities.Compte;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@RunWith(SpringRunner.class)
+@WebMvcTest(CompteController.class)
+public class CompteControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private CompteController controller;
+
+    @Test
+    public void getAll() throws Exception{
+        Compte o = new Compte();
+        o.setType("courant");
+        List<Compte> allComptes = singletonList(o);
+        given(controller.getAll()).willReturn(allComptes);
+
+        mvc.perform(get("/comptes")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].type", is(o.getType())));
+    }
+
+    @Test
+    public void getOne()throws Exception {
+        Compte o = new Compte();
+        o.setType("courant");
+        List<Compte> allComptes = singletonList(o);
+        given(controller.getAll()).willReturn(allComptes);
+
+        mvc.perform(get("/comptes")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].type", is(o.getType())));
+    }
+
+    @Test
+    public void getComptesByCompte()throws Exception {
+        Compte o = new Compte();
+        o.setType("courant");
+        List<Compte> allComptes = singletonList(o);
+        given(controller.getAll()).willReturn(allComptes);
+
+        mvc.perform(get("/comptes")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].type", is(o.getType())));
+    }
+
+    @Test
+    public void getActiveComptesByCompte()throws Exception{
+        Compte o = new Compte();
+        o.setType("courant");
+        List<Compte> allComptes = singletonList(o);
+        given(controller.getAll()).willReturn(allComptes);
+
+        mvc.perform(get("/comptes")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].type", is(o.getType())));
+    }
+
+    @Test
+    public void newOne()throws Exception{
+        Compte o = new Compte();
+        o.setType("courant");
+        List<Compte> allComptes = singletonList(o);
+        given(controller.getAll()).willReturn(allComptes);
+
+        mvc.perform(get("/comptes")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].type", is(o.getType())));
+    }
+
+    @Test
+    public void saveCompte()throws Exception{
+        Compte o = new Compte();
+        o.setType("courant");
+        List<Compte> allComptes = singletonList(o);
+        given(controller.getAll()).willReturn(allComptes);
+
+        mvc.perform(get("/comptes")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].type", is(o.getType())));
+    }
+
+    @Test
+    public void newTransaction()throws Exception{
+        Compte o = new Compte();
+        o.setType("courant");
+        List<Compte> allComptes = singletonList(o);
+        given(controller.getAll()).willReturn(allComptes);
+
+        mvc.perform(get("/comptes")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].type", is(o.getType())));
+    }
+}
